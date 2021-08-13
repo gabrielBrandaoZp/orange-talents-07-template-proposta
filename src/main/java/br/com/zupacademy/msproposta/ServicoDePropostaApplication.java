de035073a8d7a6bf2344d4cc60fd2192e079e358
Gabriel Brandão <gabriel.brandao@zup.com.br>
2021-08-13 10:31:33 -0300

    setup do projeto

---
+++ src/main/java/br/com/zupacademy/msproposta/ServicoDePropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.msproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoDePropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoDePropostaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zupacademy/msproposta/ServicoDePropostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.msproposta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoDePropostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
